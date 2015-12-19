2537d8d701e687d741fcc42a75562d5a17dbe33f
purpletianjing <purpletianjing@163.com>
2015-12-19 10:58:20 +0800

    modify 'error' to 'Input is over the range'

--- src/score-to-grade.js
+++ src/score-to-grade.js
@@ -2,7 +2,7 @@ function scoreToGrade(score) {
   var gradeRange = ['S','A','B','C','D'];
   var scoreRange = [90,80,70,60,0];
   if(score > 100 || score < 0) {
-    return 'error';
+    return 'Input is over the range';
   }
   for(var index = 0; index < scoreRange.length; index++) {
     if(score >= scoreRange[index]) {
