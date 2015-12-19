c4964d808d1f105e2142fb7ed8b3909034e1fcbb
purpletianjing <purpletianjing@163.com>
2015-12-19 20:17:51 +0800

    throw Error

--- src/score-to-grade.js
+++ src/score-to-grade.js
@@ -1,8 +1,13 @@
 function scoreToGrade(score) {
   var gradeRange = ['S','A','B','C','D'];
   var scoreRange = [90,80,70,60,0];
+
+  if(typeof(score) !== 'number') {
+    throw new TypeError("input score isn't a number!");
+  }
+
   if(score > 100 || score < 0) {
-    return 'Input is over the range';
+    return 'Input is over the range(0 - 100)';
   }
   for(var index = 0; index < scoreRange.length; index++) {
     if(score >= scoreRange[index]) {
