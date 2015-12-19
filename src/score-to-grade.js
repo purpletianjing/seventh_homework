645aca79cef1575a53b9f71dda61c167fd077b73
purpletianjing <purpletianjing@163.com>
2015-12-19 10:51:04 +0800

    simplify the programming

--- src/score-to-grade.js
+++ src/score-to-grade.js
@@ -1,17 +1,12 @@
 function scoreToGrade(score) {
-  var grade = "";
+  var gradeRange = ['S','A','B','C','D'];
+  var scoreRange = [90,80,70,60,0];
   if(score > 100 || score < 0) {
-    grade = "error";
-  }else if (score >= 90) {
-    grade = "S";
-  }else if (score >= 80) {
-    grade = "A";
-  } else if (score >= 70) {
-    grade = "B";
-  } else if (score >= 60) {
-    grade = "C";
-  } else {
-    grade = 'D';
+    return 'error';
   }
-  return grade;
+  for(var index = 0; index < scoreRange.length; index++) {
+    if(score >= scoreRange[index]) {
+      return gradeRange[index];
+    }
+  }
 }
