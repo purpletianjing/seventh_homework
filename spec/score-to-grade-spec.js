cb3db57aa6ad79640413e8e89824114fcb490aa6
purpletianjing <purpletianjing@163.com>
2015-12-19 10:57:35 +0800

    modify 'error' to 'Input is over the range'

--- spec/score-to-grade-spec.js
+++ spec/score-to-grade-spec.js
@@ -1,10 +1,10 @@
 describe("score-to-grade-spec",function() {
   it("If the score is larger than 100,the grade will be error",function(){
-    expect(scoreToGrade(102)).toBe('error');
+    expect(scoreToGrade(102)).toBe('Input is over the range');
   });
 
   it("If the score is smaller than 0,the grade will be error",function() {
-    expect(scoreToGrade(-8)).toBe('error');
+    expect(scoreToGrade(-8)).toBe('Input is over the range');
   });
 
   it("if the score is 100,the grade will be S",function() {
